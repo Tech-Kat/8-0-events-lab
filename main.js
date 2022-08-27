aa08018bb51dd5a455e6495002b41f5ade5c87a4
Tech-Kat <katpelle@pursuit.org>
2022-08-27 19:32:10 -0400

    events

--- main.js
+++ main.js
@@ -7,3 +7,28 @@ for (let i = 0; i < 100; i++) {
 }
 
 // You may write your code here!
+
+function getThemColors(e) { //e for event
+  // select current color 
+  const currentColor = document.querySelector("#current-color");
+  // check which color was clicked at the bottom , get the value 
+console.log(e.target.style.backgroundColor)
+  // change background color of current-color to that value 
+ currentColor.style.backgroundColor = e.target.style.backgroundColor;
+  
+}
+
+const color = document.querySelectorAll(".color");
+
+for(let c of color) {
+  c.addEventListener("click", getThemColors);
+}
+function paintCell(e) { // e for event
+  //currentColor must be defined in the codeblock 
+  const currentColor = document.querySelector("#current-color");
+e.target.style.backgroundColor = currentColor.style.backgroundColor
+}
+const cells = document.querySelectorAll(".cell");
+for (let cell of cells) {
+cell.addEventListener("click",paintCell)
+}
